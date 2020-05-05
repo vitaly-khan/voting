0be2f9931396c0499dba97f5d289b30efbc13a26
Vitaly Khan <swng69@yandex.ru>
2020-05-05 15:27:49 +0800

    Add ValidationUtil (id-consistency, isNew)

---
+++ src/main/java/ru/vitalykhan/voting/util/ValidationUtil.java
@@ -0,0 +1,23 @@
+package ru.vitalykhan.voting.util;
+
+import ru.vitalykhan.voting.HasId;
+import ru.vitalykhan.voting.exception.IllegalRequestDataException;
+
+public class ValidationUtil {
+    private ValidationUtil() {
+    }
+
+    public static void assureIsNew(HasId object) {
+        if (!object.isNew()) {
+            throw new IllegalRequestDataException(object + " must be new (id=null)!");
+        }
+    }
+
+    public static void assureIdConsistency(HasId object, Integer id) {
+        if (object.isNew()) {
+            object.setId(id);
+        } else if (object.getId() != id) {
+            throw new IllegalRequestDataException(object + " must be with id=" + id);
+        }
+    }
+}
