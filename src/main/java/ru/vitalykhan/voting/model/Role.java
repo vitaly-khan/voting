b9cf61c0f8a1b39a31167c5ef29f600bdfd9ca3d
Vitaly Khan <swng69@yandex.ru>
2020-05-09 12:26:52 +0800

    Provide an access to AuthenticatedUser inside application

--- src/main/java/ru/vitalykhan/voting/model/Role.java
+++ src/main/java/ru/vitalykhan/voting/model/Role.java
@@ -1,6 +1,13 @@
 package ru.vitalykhan.voting.model;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     ADMIN,
-    USER
+    USER;
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_USER";
+    }
 }
--- src/main/java/ru/vitalykhan/voting/util/SecurityUtil.java
+++ src/main/java/ru/vitalykhan/voting/util/SecurityUtil.java
@@ -1,31 +1,33 @@
 package ru.vitalykhan.voting.util;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.vitalykhan.voting.AuthenticatedUser;
+
+import java.util.Objects;
+
 public class SecurityUtil {
 
     private SecurityUtil() {
     }
 
-//    public static AuthorizedUser safeGet() {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        if (auth == null) {
-//            return null;
-//        }
-//        Object principal = auth.getPrincipal();
-//        return (principal instanceof AuthorizedUser) ? (AuthorizedUser) principal : null;
-//    }
-//
-//    public static AuthorizedUser get() {
-//        AuthorizedUser user = safeGet();
-//        requireNonNull(user, "No authorized user found");
-//        return user;
-//    }
-
-    public static int authUserId() {
-        return 100001;
-//        return get().getUserTo().id();
+    public static AuthenticatedUser safeGet() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            return null;
+        }
+        Object principal = auth.getPrincipal();
+        return (principal instanceof AuthenticatedUser) ? (AuthenticatedUser) principal : null;
     }
 
-//    public static int authUserCaloriesPerDay() {
-//        return get().getUserTo().getCaloriesPerDay();
-//    }
+    public static AuthenticatedUser get() {
+        AuthenticatedUser user = safeGet();
+        Objects.requireNonNull(user, "No authenticated user was found!");
+        return user;
+    }
+
+    public static int authUserId() {
+        return get().getId();
+    }
+
 }
