112a44ffdd40a5c41da4e82b57457108d64e8405
Vitaly Khan <swng69@yandex.ru>
2020-05-19 13:47:11 +0800

    Add IllegalOperationException (runtime)

---
+++ src/main/java/ru/vitalykhan/voting/util/exception/IllegalOperationException.java
@@ -0,0 +1,7 @@
+package ru.vitalykhan.voting.util.exception;
+
+public class IllegalOperationException extends RuntimeException {
+    public IllegalOperationException(String message) {
+        super(message);
+    }
+}
